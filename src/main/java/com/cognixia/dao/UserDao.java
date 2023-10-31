06453492a66b51bbc6752808511d1b8c56b57486
abashir191 <abdullahim.mohamed0@gmail.com>
2023-10-31 14:34:25 -0500

    dao interface

---
+++ src/main/java/com/cognixia/dao/UserDao.java
@@ -0,0 +1,35 @@
+package com.cognixia.dao;
+
+import java.util.List;
+
+public interface UserDao {
+	
+    // User operations
+    User getUserById(int userId);
+    List<User> getAllUsers();
+    User addUser(User user);
+    User updateUser(User user);
+    boolean deleteUser(int userId);
+
+    // TVShow operations
+    TVShow getTVShowById(int showId);
+    List<TVShow> getAllTVShows();
+    TVShow addTVShow(TVShow tvShow);
+    TVShow updateTVShow(TVShow tvShow);
+    boolean deleteTVShow(int showId);
+    
+    
+    // ShowGenre operations
+    ShowGenre getShowGenreById(int showGenreId);
+    List<ShowGenre> getAllShowGenres();
+    ShowGenre addShowGenre(ShowGenre showGenre);
+    ShowGenre updateShowGenre(ShowGenre showGenre);
+    boolean deleteShowGenre(int showGenreId);
+
+    // Genre operations
+    Genre getGenreById(int genreId);
+    List<Genre> getAllGenres();
+    Genre addGenre(Genre genre);
+    Genre updateGenre(Genre genre);
+    boolean deleteGenre(int genreId);
+}
